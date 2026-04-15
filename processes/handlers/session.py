cff99677404fa7b89c486a7c12223d51d8aee524
IoannaMi <59479531+IoannaMi@users.noreply.github.com>
2026-04-15 11:42:46 +0200

    Update copyright information in related files: standardize to 2025 and add SPDX license identifier; remove outdated comments and maintain code structure.

--- processes/handlers/session.py
+++ processes/handlers/session.py
@@ -1,22 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright notice
-#   --------------------------------------------------------------------
-#   Copyright (C) 2026 Deltares
-#       Gerrit Hendriksen, Ioanna Micha
-#
-#   This library is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This library is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this library.  If not, see <http://www.gnu.org/licenses/>.
-#   --------------------------------------------------------------------
+# Copyright (C) 2025 Deltares
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from pathlib import Path
 
--- processes/handlers/start.py
+++ processes/handlers/start.py
@@ -1,29 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright notice
-#   --------------------------------------------------------------------
-#   Copyright (C) 2025 Deltares
-#       Ioanna Micha, Gerrit Hendriksen
-#       ioanna.micha@deltares.nl, gerrit.hendriksen@deltares.nl
-#
-#   This library is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This library is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this library.  If not, see <http://www.gnu.org/licenses/>.
-#   --------------------------------------------------------------------
-#
-# This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
-# OpenEarthTools is an online collaboration to share and manage data and
-# programming tools in an open source, version controlled environment.
-# Sign up to recieve regular updates of this function, and to contribute
-# your own tools.
+# Copyright (C) 2025 Deltares
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import time
 import logging
